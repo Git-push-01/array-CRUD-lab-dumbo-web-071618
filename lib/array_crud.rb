88acc7b2c231ed85495a554a6dd8a40eaba37aff
Hugo Escandon <hescandon6@gmail.com>
2018-07-11 12:54:19 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,13 +11,13 @@ end
 
 def add_element_to_end_of_array(array, element)
 
-  ary.push("e")
+  ary.push(elment)
 
 end
 
 def add_element_to_start_of_array(array, element)
   
-  ary.unshift("z")
+  ary.unshift(element)
 
 end
 
